da36e6fdf7edfc1e82bdcdfa925e3c424f6a85aa
Wenxuan Wu <wwx970624771@gmail.com>
2023-09-28 11:49:06 -0700

    cloud access address updated

--- src/api/apiAddress.js
+++ src/api/apiAddress.js
@@ -1,3 +1,3 @@
-const API_URL = 'http://localhost:8000'; // Replace with your actual API address
+const API_URL = 'http://54.196.149.187:8000'; // Replace with your actual API address
 
 export default API_URL;
