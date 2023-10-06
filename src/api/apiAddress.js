f22557a682e774ccded0f4b432c8502e97e82df7
Pengyu Zhang <pezhang036@gmail.com>
2023-10-06 17:47:58 -0500

    url fix

--- src/api/apiAddress.js
+++ src/api/apiAddress.js
@@ -1,5 +1,4 @@
-// const API_URL = 'http://54.196.149.187:8000'; // Replace with your actual API address
-//const API_URL = 'http://localhost:8000';
-const API_URL = "54.196.149.187"
+const API_URL = 'http://54.196.149.187:8000'; // Replace with your actual API address
+// const API_URL = 'http://localhost:8000';
 export const MIDDLEWARE_URL = 'http://localhost:8000'
 export default API_URL;
