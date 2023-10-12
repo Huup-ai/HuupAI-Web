eab981dccac283c1d69ebb264b40505e3d2b6f69
Wenxuan Wu <wwx970624771@gmail.com>
2023-10-11 17:40:03 -0700

    update Consumer Profile, fix signup

--- src/components/UpdateBank.jsx
+++ src/components/UpdateBank.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React from 'react'
 import { useStateContext } from "../contexts/ContextProvider";
 import Button from "./Button";
 
-const InvoiceInfo = () => {
-  const { currentColor } = useStateContext();
+const UpdateBank = () => {
+    const { currentColor } = useStateContext();
   return (
     <div className="border-2 rounded-xl w-1/2">
-      <h6 className="md:p-2">Company Info(optional): </h6>
+      <h6 className="md:p-2">Update Bank Info(optional): 
+      <br/>
+      Note: we only support US Bank. For international customers, we will deposit crypto to your default wallet.
+      <br/>
+      Please use moonpay to swap that to your local fiat at the invoice page </h6>
+      
 
       <div className="flex flex-col items-center justify-between">
         <div className="flex flex-col flex-auto md:px-5 md:py-5 gap-2 w-full">
           <div className='flex justify-between items-center'>
-            <label> Company Name : </label>
+            <label> Routing Number : </label>
             <input 
             className='border-2 p-2 rounded w-2/3'
             type='text'
@@ -19,7 +24,7 @@ const InvoiceInfo = () => {
           </div>
 
           <div className='flex justify-between items-center'>
-            <label>Company Address:</label>
+            <label>Account Number:</label>
             <input 
             className='border-2 p-2 rounded w-2/3'
             type='text'
@@ -27,14 +32,6 @@ const InvoiceInfo = () => {
 
           </div>
 
-          <div className='flex justify-between items-center'>
-            <label>EIN:</label>
-            <input 
-            className='border-2 p-2 rounded w-2/3'
-            type='text'
-            />
-
-          </div>
         </div>
         <div className="md:mb-5 ">
           <Button
@@ -50,4 +47,4 @@ const InvoiceInfo = () => {
   )
 }
 
-export default InvoiceInfo
+export default UpdateBank
--- src/components/UpdatePwd.jsx
+++ src/components/UpdatePwd.jsx
@@ -1,9 +1,103 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { useStateContext } from "../contexts/ContextProvider";
+import Button from "./Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 const UpdatePwd = () => {
+  const { currentColor } = useStateContext();
+  const [password_old, setPassword_old] = useState("");
+  const [password_new, setPassword_new] = useState("");
+  const [password_con, setPassword_con] = useState("");
+  const [showPassword_old, setShowPassword_old] = useState(false); // password visibility
+  const toggleShowPassword_old = () => {
+    setShowPassword_old(!showPassword_old);
+  };
+  const [showPassword_new, setShowPassword_new] = useState(false); // password visibility
+  const toggleShowPassword_new = () => {
+    setShowPassword_new(!showPassword_new);
+  };
+  const [showPassword_con, setShowPassword_con] = useState(false); // password visibility
+  const toggleShowPassword_con = () => {
+    setShowPassword_con(!showPassword_con);
+  };
   return (
-    <div>UpdatePwd</div>
-  )
-}
+    <div className="border-2 rounded-xl w-1/2">
+      <h6 className="md:p-2">Update Password: </h6>
 
-export default UpdatePwd
+      <div className="flex flex-col items-center justify-between">
+        <div className="flex flex-col flex-auto md:px-5 md:py-5 gap-2 w-full">
+          <div className="flex justify-between items-center">
+            <label> Old Password: </label>
+            <div className="flex w-2/3">
+              <input
+                type={showPassword_old ? "text" : "password"}
+                className="border-2 p-2 rounded w-full"
+                name="password"
+                value={password_old}
+                onChange={(e) => setPassword_old(e.target.value)}
+              />
+              <button
+                type="button"
+                onClick={toggleShowPassword_old}
+                className="md:ml-5"
+              >
+                <FontAwesomeIcon icon={showPassword_old ? faEyeSlash : faEye} />
+              </button>
+            </div>
+          </div>
+          <div className="flex justify-between items-center">
+            <label> New Password: </label>
+            <div className="flex w-2/3">
+              <input
+                type={showPassword_new ? "text" : "password"}
+                className="border-2 p-2 rounded w-full"
+                name="password"
+                value={password_new}
+                onChange={(e) => setPassword_new(e.target.value)}
+              />
+              <button
+                type="button"
+                onClick={toggleShowPassword_new}
+                className="md:ml-5"
+              >
+                <FontAwesomeIcon icon={showPassword_new ? faEyeSlash : faEye} />
+              </button>
+            </div>
+          </div>
+          <div className="flex justify-between items-center">
+            <label> Confirm Password: </label>
+            <div className="flex w-2/3">
+              <input
+                type={showPassword_con ? "text" : "password"}
+                className="border-2 p-2 rounded w-full"
+                name="password"
+                value={password_con}
+                onChange={(e) => setPassword_con(e.target.value)}
+              />
+              <button
+                type="button"
+                onClick={toggleShowPassword_con}
+                className="md:ml-5"
+              >
+                <FontAwesomeIcon icon={showPassword_con ? faEyeSlash : faEye} />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="md:mb-5 ">
+          <Button
+            color="white"
+            bgColor={currentColor}
+            text="Update"
+            // onClickCallback={handleDeposit}
+            borderRadius="10px"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UpdatePwd;
