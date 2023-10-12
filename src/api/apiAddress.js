c1c42d7833e6a358376d64326f0e0875b7b87e49
Pengyu Zhang <pezhang036@gmail.com>
2023-10-12 17:21:56 -0500

    adaptive base url, defined in .env

--- src/api/apiAddress.js
+++ src/api/apiAddress.js
@@ -1,4 +1,5 @@
-const API_URL = 'http://54.196.149.187:8000'; // Replace with your actual API address
+// const API_URL = 'http://54.196.149.187:8000'; // Replace with your actual API address
 // const API_URL = 'http://localhost:8000';
+const API_URL = process.env.REACT_APP_BACKEND_BASE_URL
 export const MIDDLEWARE_URL = 'http://localhost:8000'
 export default API_URL;
