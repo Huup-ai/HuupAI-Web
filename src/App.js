11c8b61a3a8b478acf382c03014483acbe042063
christsoi <christstststs@gmail.com>
2023-10-19 14:56:27 -0700

    Fixed CSS styles' bugs

--- src/App.js
+++ src/App.js
@@ -29,8 +29,8 @@ function App() {
 
   return (
     <div className="App">
-      <div className="blur" style={{ top: "-18%", right: "0" }}></div>
-      <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
+      {/* <div className="blur" style={{ top: "-18%", right: "0" }}></div>
+      <div className="blur" style={{ top: "36%", left: "-8rem" }}></div> */}
       <Routes>
       <Route path="/" element={<Landing />} /> 
       <Route path="/signup" element={ <SignUp />} />
