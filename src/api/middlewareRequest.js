f5d9166d9083478b9080c72f779d4671eb0b80e2
corazhangjob <148160857+corazhangjob@users.noreply.github.com>
2023-10-25 14:17:06 -0400

    Update middlewareRequest.js

--- src/api/middlewareRequest.js
+++ src/api/middlewareRequest.js
@@ -1,9 +1,7 @@
-// import { MIDDLEWARE_URL } from "./apiAddress";
+import { MIDDLEWARE_URL } from "./apiAddress";
 import { FetchRequest } from "./index";
 
 export async function getCryptoPayment(wallet_address) {
-  const MIDDLEWARE_URL = "http://54.196.149.187:8080";
-  console.log("MIDDLEWARE_URL:", MIDDLEWARE_URL);
   return FetchRequest(
     `${MIDDLEWARE_URL}/contract/getCryptoPayment/`,
     "POST",
