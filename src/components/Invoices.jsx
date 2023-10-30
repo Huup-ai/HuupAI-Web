6646eb0e34bbef19308cd2b886b219cfb690013a
christsoi <christstststs@gmail.com>
2023-10-30 15:56:47 -0700

    Edit Provider Invoices

--- src/components/Invoices.jsx
+++ src/components/Invoices.jsx
@@ -1,89 +1,5 @@
-// import React, { useState, useEffect } from "react";
-// import { InvoicesGrid } from "../data/dummy";
-// import {
-//   GridComponent,
-//   Inject,
-//   ColumnsDirective,
-//   ColumnDirective,
-//   Search,
-//   Page,
-//   Toolbar,
-// } from "@syncfusion/ej2-react-grids";
-
-// import { generateInvoice } from "../api";
-
-// const Invoices = () => {
-//   const toolbarOptions = ["Search"];
-
-//   const editing = { allowDeleting: true, allowEditing: true };
-//   const settings = { wrapMode: "Content" };
-//   const [invoice, setInvoice] = useState(null);
-
-//   useEffect(() => {
-//     generateInvoice()
-//       .then((generatedInvoice) => {
-//         setInvoice(generatedInvoice);
-//       })
-//       .catch((error) => {
-//         console.error("Failed to generate invoice:", error);
-//       });
-//   }, []);
-
-//   return (
-//     <div id="Invoices">
-//       <div className=" mb-5 mt-10">
-//         <p className="text-2xl font-extrabold tracking-tight text-slate-900">
-//           Invoices
-//         </p>
-//         <p className="text-lg text-gray-400">
-//           Itemized statements of service charges
-//         </p>
-//       </div>
-//       <div className="drop-shadow-[0px_5px_5px_rgba(0,0,0,0.15)]">
-//         <GridComponent
-//           rowHeight={100}
-//           dataSource={InvoicesData}
-//           width="auto"
-//           allowPaging
-//           allowSorting
-//           pageSettings={{ pageCount: 5 }}
-//           editSettings={editing}
-//           toolbar={toolbarOptions}
-//           allowTextWrap={true}
-//           textWrapSettings={settings}
-//         >
-//           <ColumnsDirective>
-//             {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-//             {InvoicesGrid.map((item, index) => (
-//               <ColumnDirective
-//                 clipMode="EllipsisWithTooltip"
-//                 key={index}
-//                 {...item}
-//               />
-//             ))}
-
-//             <ColumnDirective
-//               headerText="Actions"
-//               template={record => record.status === "Unpaid" ? <button>Action</button> : null}
-//   />
-//           </ColumnsDirective>
-//           <Inject services={[Search, Page, Toolbar]} />
-//         </GridComponent>
-//       </div>
-
-//       {invoice && (
-//         <div>
-//           <h2>Invoice</h2>
-//           <p>Date: {invoice.date}</p>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Invoices;
-
 import React, { useState, useEffect } from "react";
+// import { InvoicesGrid } from "../data/dummy";
 import {
   GridComponent,
   Inject,
@@ -93,54 +9,73 @@ import {
   Page,
   Toolbar,
 } from "@syncfusion/ej2-react-grids";
-import { InvoicesGrid } from "../data/dummy";
-import { getInvoiceByUser, generateInvoice } from "../api";
-import Web3 from "web3";
+import { generateInvoice } from "../api";
+import { BsDownload } from "react-icons/bs";
+import Button from "../components/Button";
 
 const Invoices = () => {
   const toolbarOptions = ["Search"];
   const editing = { allowDeleting: true, allowEditing: true };
   const settings = { wrapMode: "Content" };
-
-  // We will store the fetched invoices in this state
-  const [invoicesData, setInvoicesData] = useState([]);
+  const [invoice, setInvoice] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    getInvoiceByUser()
-      .then((fetchedInvoices) => {
-        setInvoicesData(fetchedInvoices);
+    generateInvoice()
+      .then((generatedInvoice) => {
+        setInvoice(generatedInvoice);
+        setLoading(false);
       })
       .catch((error) => {
-        console.error("Failed to fetch invoices:", error);
+        console.error("Failed to generate invoice:", error);
+        setLoading(false);
       });
   }, []);
 
-  // Set up web3 and the contract
-  const web3 = new Web3(Web3.givenProvider); // uses MetaMask's provider
-  // const contractAbi = ...;  // ABI from your contract
-  const contractAddress = "0xYourContractAddress";
-  // const contract = new web3.eth.Contract(contractAbi, contractAddress);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-  // const withdrawEarnings = async () => {
-  //   try {
-  //     // Note: You might want to first ensure the user is connected to a wallet provider, e.g., MetaMask
-  //     const accounts = await web3.eth.getAccounts();
-  //     if (accounts.length === 0) {
-  //       alert("Please connect to MetaMask.");
-  //       return;
-  //     }
+  const gridInvoice = () => (
+    <div className="flex items-center justify-center gap-2">
+      <Button 
+        icon={<BsDownload />}
+        onClickCallback={() => {
+          const link = document.createElement("a");
+          // link.href = downloadUrl;
+          // link.download = fileName;
+          link.click();
+        }}
+      />
+    </div>
+  );
 
-  //     const invoice = await generateInvoice();
-  //     const earning = invoice.total_price;  // assuming total_price is the earning
-
-  //     await contract.methods.withdrawEarning(earning).send({ from: accounts[0] });
-  //     alert('Earnings withdrawn successfully!');
-
-  //   } catch (error) {
-  //     console.error("Error withdrawing earnings:", error);
-  //     alert("Error withdrawing earnings. Check console for details.");
-  //   }
-  // };
+  const InvoicesGrid = [
+    {
+      field: "date",
+      headerText: "DATE",
+      width: "100",
+      textAlign: "Center",
+    },
+    {
+      field: "des",
+      headerText: "INSTANCE DES",
+      width: "200",
+      textAlign: "Center"
+    },
+    {
+      headerText: "INVOICE",
+      width: "100",
+      template: gridInvoice,
+      textAlign: "Center",
+    },
+    {
+      field: "status",
+      headerText: "STATUS",
+      width: "100",
+      textAlign: "Center"
+    },
+  ];
 
   return (
     <div id="Invoices">
@@ -155,7 +90,7 @@ const Invoices = () => {
       <div className="drop-shadow-[0px_5px_5px_rgba(0,0,0,0.15)]">
         <GridComponent
           rowHeight={100}
-          dataSource={invoicesData}
+          dataSource={invoice}
           width="auto"
           allowPaging
           allowSorting
@@ -166,21 +101,17 @@ const Invoices = () => {
           textWrapSettings={settings}
         >
           <ColumnsDirective>
-            {InvoicesGrid.map((item) => (
+            {InvoicesGrid.map((item, index) => (
               <ColumnDirective
-                key={item.field}
-                field={item.field}
-                headerText={item.headerText}
-                width={item.width}
-                textAlign={item.textAlign}
-                isPrimaryKey={item.isPrimaryKey}
+                clipMode="EllipsisWithTooltip"
+                key={index}
+                {...item}
               />
             ))}
-            {/* <ColumnDirective
+            <ColumnDirective
               headerText="Actions"
-              template={record => record.status === "Unpaid" ? 
-              <button onClick={withdrawEarnings}>Unpaid</button> : null}
-            /> */}
+              template={record => record.status === "Unpaid" ? <button>Action</button> : null}
+            />
           </ColumnsDirective>
           <Inject services={[Search, Page, Toolbar]} />
         </GridComponent>
@@ -190,3 +121,5 @@ const Invoices = () => {
 };
 
 export default Invoices;
+
+
